32b8504b3c1c9a1aacf5b6d8be29e0c0bee8f505
DomenickCE <43227526+DomenickCE@users.noreply.github.com>
2018-10-10 02:32:25 -0400

    holes data progress

--- Holes/app.js
+++ Holes/app.js
@@ -13,7 +13,6 @@ mongoose.connection.once("open", function(){
     console.log(error);
 });
 
-
 var holeSchema = new mongoose.Schema({
             
     black: Number,
@@ -29,8 +28,6 @@ var holeSchema = new mongoose.Schema({
 
 });
 
-
-
 var holes = mongoose.model('holes', holeSchema);
 
 var options = {
@@ -41,16 +38,24 @@ var options = {
     },
     json:true
 }
+
+
+
 function callback(err, request, body){
-    if(err){ 
-        
+    if(err){       
      console.log(err)
  } 
- else{ 
+ else{  
+   
+    const request = require("request")
+
     var courseBody = body
  
     var length = courseBody.courses.length
-  console.log(length)
+
+    console.log(length)
+    
+    
 
           // it is not good to hit the server with a lot of requests which will overload it. (looping the request 90+ times)
     for(var i = 0; i < length; i++){
@@ -60,25 +65,52 @@ function callback(err, request, body){
             headers: {
                 'Authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhcHAuZTZnb2xmLmNvbSIsImV4cCI6MTU2NTkzNjI4MCwiaWF0IjoxNTM3MTM2MjgwLCJpc3MiOiJUcnVHb2xmIEFQSSIsImp0aSI6IjBmZmFmOTE4LWE3ZjUtNGI1YS1hY2YxLTVjMTI2YjExYTg0OSIsInNjb3BlcyI6WyJhcGk6YWNjZXNzIiwiYXBpOnBsYXllcjphY2Nlc3MiXSwic3ViIjoxMDQ2OTIsInN1YlR5cGUiOiJtb2RlbHMudXNlciIsInVzZXJJRCI6MTA0NjkyfQ.ltTXF_vfZYo97BxYi-aUiAJMemr71wVhDuhb85weYWU"
             },
-            json:true,
+            json:true
         }
         function callback2(err, request, body){
             if(err){
                 console.log(err);
             }
             else{
-                var length = courseBody.courses.length
+               
                 var Options2Data = body;
-    
-                
-                console.log(length);
-    
-            
+
+                console.log(Options2Data.holes.length)
+
+               var HolesLength = Options2Data.holes.length
+
+                for(var i = 0; i < HolesLength; i++){
+                holes.insertMany([
+                    {
+                    black: Options2Data.holes[i].black,
+                    blue:Options2Data.holes[i].blue,
+                    courseID: Options2Data.holes[i].courseID,
+                    gold: Options2Data.holes[i].gold,
+                    handicap: Options2Data.holes[i].handicap,
+                    holeImageURL: Options2Data.holes[i].holeImageURL,
+                    holeNumber: Options2Data.holes[i].holeNumber,
+                    par: Options2Data.holes[i].par,
+                    red: Options2Data.holes[i].red,
+                    white: Options2Data.holes[i].white,
+                    }
+                 ], function(err){
+                     if(err)console.log("error saving to database:" + err)
+
+                     else{console.log("Saving to Database")}
+                 });
+
+                    }
+                           
                 };
                     
             };
+            
+
+            request(options2,callback2);
+
+            
      }
- 
+    
  }
 
 
@@ -89,21 +121,8 @@ function callback(err, request, body){
      
     };// loop
        
-    // holes.insertMany([
-    //     {
-    //     black: Number,
-    //     blue: Number,
-    //     courseID: Number,
-    //     gold: Number,
-    //     handicap: Number,
-    //     holeImageURL: String,
-    //     holeNumber: Number,
-    //     par: Number,
-    //     red: Number,
-    //     white: Number,
-    // }
-    //  ]);
+    
 
-    request(options2,callback2);
+    
     //req, res
     request(options,callback);
